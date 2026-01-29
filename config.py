432865ba7dee9e0d004dfd18c074ceca45b6f726
tlgb <tlgbkakaka@gmail.com>
2026-01-29 10:40:57 +0700

    done extract to temp db

--- config.py
+++ config.py
@@ -1,7 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 TEMP_DB = {
-    "host": "localhost",
-    "port": 5432,
-    "database": "temp_db",
-    "user": "postgres",
-    "password": "170723"
+    "host": os.getenv("TEMP_DB_HOST"),
+    "port": os.getenv("TEMP_DB_PORT"),
+    "dbname": os.getenv("TEMP_DB_NAME"),
+    "user": os.getenv("TEMP_DB_USER"),
+    "password": os.getenv("TEMP_DB_PASSWORD"),
 }
